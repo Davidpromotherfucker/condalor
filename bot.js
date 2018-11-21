f86b9a0b42bb586353f06410b7e6f4eb04dd26ac
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-21 22:27:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "c";
-var adminprefix = 'c'
+var prefix = "S";
+var adminprefix = 'S'
 
-const developers = ["472413769700474901","id"]
+const developers = ["498625669580980224"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'setg')) {
+  if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
       message.channel.send(`**Now PLaying ${argresult}**`)
   } else 
